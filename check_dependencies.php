e1b6bd126cd108b0f83b146b510728fd898f52ad
isakbh <isak@brunhenriksen.net>
2026-05-14 20:57:27 +0200

    dependency sjekken funker nå

--- check_dependencies.php
+++ check_dependencies.php
@@ -1,10 +1,12 @@
 <?php
-$sjekk_scrcpy_dependency = shell_exec('scrcpy --version');
-if(str_contains($sjekk_scrcpy_dependency, "not found")){
+$sjekk_scrcpy_dependency = shell_exec('command -v scrcpy 2>&1');
+if(empty($sjekk_scrcpy_dependency)){
     echo "Du har ikke scrcpy installert på maskinen din. For at dette scriptet skal funke, må du installere det.";
+    exit;
 }
 
-$sjekk_avahi_dependency = shell_exec('avahi-browse --version');
-if(str_contains($sjekk_avahi_dependency, "not found")){
+$sjekk_avahi_dependency = shell_exec('command -v avahi-browse 2>&1');
+if(empty($sjekk_avahi_dependency)){
     echo "Du har ikke avahi installert på maskinen din. For at dette scriptet skal funke, må du installere det.";
+    exit;
 }
