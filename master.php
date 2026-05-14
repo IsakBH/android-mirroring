5f2a411c00c4b7932f9b2c1ad8b30166e6866c62
isakbh <isak@brunhenriksen.net>
2026-05-14 20:20:50 +0200

    kjører nå scrcpy for å få speiling av skjerm

--- master.php
+++ master.php
@@ -2,7 +2,11 @@
 include "variables.php";
 
 $kommando_for_tilkobling = "adb connect $ip_adresse:$debugging_port";
+$scrcpy_kommando = "scrcpy";
 
 if(shell_exec($kommando_for_tilkobling)){
     echo "Tilkoblet! \n";
+
+    echo "Kjører scrcpy for å speile skjermen... \n";
+    shell_exec($scrcpy_kommando);
 }
